18084aaf48daf9f685dcda5f79ff21dad65e57b6
Jesús Oviedo Riquelme <j92riquelme@gmail.com>
2020-11-11 00:45:03 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,18 @@ from datetime import date
 
 # Set Date format like: YYYY, MM, DD
 # Note that FIND_FIRST_DATE uses START_DATE as default start date
-START_DATE = date(1990, 1, 1)
-END_DATE = date(2020, 7, 19)
+
+today = date.today()
+year_start = 2020
+month_start = 3
+day_start = 7
+
+START_DATE = date(year_start, month_start, day_start)
+END_DATE = date(today.year, today.month, today.day)
 
 # set to "metric" or "imperial"
 UNIT_SYSTEM = "metric"
 # UNIT_SYSTEM = "imperial"
 
 # Automatically find first date where data is logged
-FIND_FIRST_DATE = True
+FIND_FIRST_DATE = False
